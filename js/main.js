30b1138e452b895562aed6b6da6055cbde5a06f9
EricMareth <EricMareth@fullsail.edu>
2013-03-13 16:45:53 -0400

    fixed validation, now new errors need to be fixed before moving to storage

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@
 // Character Note CRUD Application
 
 $(document).on('pageinit',function(){
+	
 	var imageForm = $('#newImage');
-	imageForm.validate();
+		errorDialog = $('#errorDialog');
+	imageForm.validate({
+		invalidHandler: function(form, validator){
+			errorDialog.click();
+			var html = "";
+			for (var key in validator.submitted){
+				var label = $('label[for^="'+ key +'"]').not('[generated]');
+				var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+				var fieldName = legend.length ? legend.text() : label.text();
+				html += '<li>'+ fieldName +'</li>';
+			};
+			$("#formErrors p").html(html);
+			html="";
+		},
+		submitHandler: function(){
+		
+		}
+	
+	});
 });
